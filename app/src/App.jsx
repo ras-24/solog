832acf04ed6783a2b8dafe55c5da6a4ec71713ef
Ras <siregar.riyando@gmail.com>
2024-08-13 16:32:41 +0800

    Adding connection & wallet provider

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,23 @@
-import { useMemo } from "react"
-import { BlogProvider } from "src/context/Blog"
-import { Router } from "src/router"
-import "./App.css"
-
+import { useMemo } from "react";
+import { BlogProvider } from "src/context/Blog";
+import { Router } from "src/router";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import "./App.css";
 
 export const App = () => {
+  const endpoint = "https://omniscient-virulent-arm.solana-devnet.quiknode.pro/295ac518ec29a4047335d3a2b02cca7c156eb99a";
 
   return (
-    <BlogProvider>
-      <Router />
-    </BlogProvider>
-  )
-}
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider>
+        <BlogProvider>
+          <Router />
+        </BlogProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
