0e83274e5d8cad7dc70114d01be193b22f47b300
Ras <siregar.riyando@gmail.com>
2024-08-19 11:25:51 +0800

    Destructuring

--- app/src/context/Blog.jsx
+++ app/src/context/Blog.jsx
@@ -1,6 +1,16 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useMemo, useEffect, useState } from "react";
+import * as anchor from '@project-serum/anchor';
+import { useAnchorWallet, useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
+import { getAvatarUrl } from "src/functions/getAvatarUrl";
+import { getRandomName } from "src/functions/getRandomName";
+import idl from "src/idl.json";
+import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey";
+import { utf8 } from "@project-serum/anchor/dist/cjs/utils/bytes";
 
 const BlogContext = createContext();
+
+const PROGRAM_KEY = new PublicKey(idl.metadata.address)
 
 export const useBlog = () => {
   const context = useContext(BlogContext);
@@ -13,17 +23,45 @@ export const useBlog = () => {
 
 export const BlogProvider = ({ children }) => {
 
-  const user = {
-    name: "Dog",
-    avatar:
-      "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?q=80&w=1434&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  };
+  const [user, setUser] = useState()
+  const [initialized, setInitialized] = useState(false)
 
+  const anchorWallet = useAnchorWallet();
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+
+  const program = useMemo(() => {
+    if (anchorWallet) {
+      const provider = new anchor.AnchorProvider(connection, anchorWallet, anchor.AnchorProvider.defaultOptions())
+      return new anchor.Program(idl, PROGRAM_KEY, provider)
+    }
+  }, [connection, anchorWallet])
+
+  useEffect(() => {
+    const start = async () => {
+      if (program && publicKey) {
+        try {
+          // Check if there is a user account
+          const [userPda] = await findProgramAddressSync([utf8.encode('user'), publicKey.toBuffer()], program.programId())
+          const user = await program.account.userAccount.fetch(userPda)
+          if (user) {
+            setInitialized(true) // Create Post
+          }
+        } catch (err) {
+          console.log("No User")
+          setInitialized(false) // Initialize user
+        }
+      }
+    }
+
+    start()
+  }, [])
 
   return (
     <BlogContext.Provider
       value={{
-        user
+        user,
+        initialized
       }}
     >
       {children}
